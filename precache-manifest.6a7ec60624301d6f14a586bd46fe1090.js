e1ee2295fb2c6272657836f6c1096a136e85fd7c
faltastic <youssef.faltas@gmail.com>
2019-06-07 02:29:16 +0200

    Updates

---
+++ precache-manifest.6a7ec60624301d6f14a586bd46fe1090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab275d677181fd38b631",
+    "url": "/sheetr/static/js/runtime~main.ab275d67.js"
+  },
+  {
+    "revision": "0c387df392900c986ea0",
+    "url": "/sheetr/static/js/main.0c387df3.chunk.js"
+  },
+  {
+    "revision": "13d708e429479ee6eddf",
+    "url": "/sheetr/static/js/2.13d708e4.chunk.js"
+  },
+  {
+    "revision": "0c387df392900c986ea0",
+    "url": "/sheetr/static/css/main.f3b35968.chunk.css"
+  },
+  {
+    "revision": "13d708e429479ee6eddf",
+    "url": "/sheetr/static/css/2.272826d2.chunk.css"
+  },
+  {
+    "revision": "c1a46637fbbf336be2a1ae84772600bc",
+    "url": "/sheetr/index.html"
+  }
+];
